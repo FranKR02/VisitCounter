ef03abf54fc38630a85d2c021c66eb8adc312720
FranKR02 <frankdiego007@hotmail.com>
2024-02-20 07:28:47 -0500

    Program  settings

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using VisitCounter.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapHub<VisitHub>("/visitHub");
+
+app.Run();
