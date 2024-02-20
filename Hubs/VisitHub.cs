039f78bdccb91df50e64ab6c7820699c489629b1
FranKR02 <frankdiego007@hotmail.com>
2024-02-20 07:29:28 -0500

    Class Hub

---
+++ Hubs/VisitHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace VisitCounter.Hubs
+{
+    public class VisitHub : Hub
+    {
+        private static int connections = 0;
+        public async Task CountVisitors()
+        {
+            Interlocked.Increment(ref connections);
+            // Envía el conteo de visitantes a todos los clientes
+            await Clients.All.SendAsync("ReceiveVisitorsCount", connections);
+        }
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.AllExcept(Context.ConnectionId).SendAsync("NewConnection", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Clients.All.SendAsync("Connectionclose: " + Context.ConnectionId);
+        }
+    }
+}
